bdd10d14653987d0c4f3307ea84983e57eb8ed0e
Sujith H <sharidasan@owncloud.com>
2018-03-13 21:19:29 +0530

    A new exception to suppress exception logs
    There are scenarios where an app would have
    done the job and the logger would have logged.
    But the main task would throw an exception.
    Since its logged gracefully we need not throw
    exception in the log. This change would help
    for the same.
    
    Signed-off-by: Sujith H <sharidasan@owncloud.com>

--- apps/dav/tests/unit/Connector/Sabre/DirectoryTest.php
+++ apps/dav/tests/unit/Connector/Sabre/DirectoryTest.php
@@ -26,6 +26,7 @@ namespace OCA\DAV\Tests\Unit\Connector\Sabre;
 
 use OC\Files\FileInfo;
 use OCA\DAV\Connector\Sabre\Directory;
+use OCP\Files\ExcludeForbiddenException;
 use OCP\Files\ForbiddenException;
 
 class TestDoubleFileView extends \OC\Files\View {
@@ -430,5 +431,21 @@ class DirectoryTest extends \Test\TestCase {
 			->willReturn(true);
 
 		$targetNode->moveInto(basename($destination), $source, $sourceNode);
+	}
+
+	/**
+	 * A test to throw ExcludeForbiddenException
+	 *
+	 * @expectedException \OCP\Files\ExcludeForbiddenException
+	 * @expectedExceptionMessage The message already logged
+	 */
+	public function testFailCreateFile() {
+		//$this->invokePrivate();
+		$previous = new ExcludeForbiddenException('The message already logged', false);
+		$this->view->expects($this->any())
+			->method('isCreatable')
+			->willThrowException(new ExcludeForbiddenException('The message already logged', false, $previous));
+		$dir = $this->getDir();
+		$dir->createFile('foobar.txt', 'hello foo bar');
 	}
 }
